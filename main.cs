c3b7376933537bdfc5cffdb997a4ab663450c1b8
Craig Barkley <37020436+tektechnologies@users.noreply.github.com>
2024-10-16 02:34:50 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,34 @@
+using System;
+
+class Program {
+  public static void Main(String[] args)
+  {
+    int[] arr = {10, 7, 3, 1, 9, 7, 4, 3};
+    
+    Console.Write("Initial Array : ");
+    
+    Console.WriteLine(String.Join(" ", arr));
+    
+    Sort(arr);
+  }
+
+  static void Sort(int[] arr)
+  {
+    for (int i = 0; i < arr.Length; i++)
+    {
+      for (int j = 0; j < arr.Length - i - 1; j++)
+      {
+        if (arr[j] > arr[j + 1])
+        {
+          int temp = arr[j];
+          arr[j] = arr[j + 1];
+          arr[j + 1] = temp;
+        }
+      }
+
+      Console.Write("After pass " + i + "  : ");
+      Console.WriteLine(String.Join(" ", arr));
+    }
+  }
+}
+
